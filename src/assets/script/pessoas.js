43812774ef900b82fa4ede43280701f97de43b52
Cristian Breno Andrade Marques <1470341@sga.pucminas.br>
2024-06-03 21:55:27 -0300

    fazendo as funcionalidades do site

--- src/assets/script/pessoas.js
+++ src/assets/script/pessoas.js
@@ -26,64 +26,69 @@ function leituraDadosUsuarios() {
 var telaPessoas = document.querySelector(".telaPessoas");
 var inputPessoas = document.querySelector("#inputPessoas");
 var btnFiltrar = document.querySelector("#filtrarPessoas");
-
-btnFiltrar.addEventListener("click", ()=>{
-
-
+let resultadoSearch = [];
+btnFiltrar.addEventListener("click", () => {
     let empresasObjeto = leituraDadosEmpresas();
     let usuariosObjeto = leituraDadosUsuarios();
     telaPessoas.innerHTML = '';
+
     empresasObjeto.forEach(eachEmpresa => {
-        if(eachEmpresa.nomeFantasia === inputPessoas.value){
+        if (eachEmpresa.nomeFantasia === inputPessoas.value) {
             let defaultHTML = `
-        <div class="resultadoPesquisa">
-            <span id="info">
-                <a href="#"><img src="assets/img/icones de interface/do-utilizador (2).png" /></a>
-                <h2>${eachEmpresa.nomeFantasia}</h2>
-            </span>
-            <button id="adicionarPessoa"data-id=${eachEmpresa.id}>Adicionar</button>
-        </div>`;
+                    <div class="resultadoPesquisa">
+                        <span id="info">
+                            <a href="#"><img src="assets/img/icones de interface/do-utilizador (2).png" /></a>
+                            <h2>${eachEmpresa.nomeFantasia}</h2>
+                        </span>
+                        <button class="adicionarPessoa" data-id="${eachEmpresa.id}">Adicionar</button>
+                    </div>`;
             telaPessoas.innerHTML += defaultHTML;
+            resultadoSearch.push(eachEmpresa.id);
         }
     });
-    //script para adicionar amigos
-    document.querySelectorAll(".adicionarPessoa").forEach(function(eachBtn){
-        eachBtn.addEventListener("click", function(){
-            console.log("sigma")
-            var empresaId = parseInt(eachBtn.dataset.id);
-            var empresaSelecionada = empresasObjeto.findIndex(empresas => empresas.id === empresaId);
 
-            // Verifica se o botão já está selecionado
-            if (eachBtn.style.backgroundColor === "rgba(0, 128, 17, 0.849)") {
-                eachBtn.style.backgroundColor = "";
-                console.log("Empresa deselecionada:", empresasObjeto[empresaSelecionada]);
-                // Remove a empresa selecionada de alguma lista, se necessário
-            } else {
-                eachBtn.style.backgroundColor = "rgba(0, 128, 17, 0.849)";
-                console.log("Empresa selecionada:", empresasObjeto[empresaSelecionada]);
-                // Adiciona a empresa selecionada a alguma lista, se necessário
-            }
+    usuariosObjeto.forEach(eachUser => {
+        if (eachUser.nome === inputPessoas.value) {
+            let defaultHTML = `
+                    <div class="resultadoPesquisa">
+                        <span id="info">
+                            <a href="#"><img src="assets/img/icones de interface/do-utilizador (2).png" /></a>
+                            <h2>${eachUser.nome}</h2>
+                        </span>
+                        <button class="adicionarPessoa" data-id="${eachUser.id}">Adicionar</button>
+                    </div>`;
+            telaPessoas.innerHTML += defaultHTML;
+            resultadoSearch.push(eachUser.id);
+        }
+    });
+    document.querySelectorAll(".adicionarPessoa").forEach(function (eachBtn) {
+        eachBtn.addEventListener("click", function () {
+            var pessoaId = parseInt(eachBtn.dataset.id);
+            var pessoaSelecionada = resultadoSearch.findIndex(resultadoSearch => resultadoSearch === pessoaId);
+            console.log(pessoaSelecionada)
+            console.log(resultadoSearch)
         });
     });
-    // usuariosObjeto.forEach(eachUsers => {
-    //     if(eachUsers.nome === inputPessoas.value){
-    //         let defaultHTML = `
-    //     <div class="resultadoPesquisa">
-    //         <span id="info">
-    //             <a href="#"><img src="assets/img/icones de interface/do-utilizador (2).png" /></a>
-    //             <h2>${eachUsers.nome}</h2>
-    //         </span>
-    //         <button id="adicionarPessoa">Adicionar</button>
-    //     </div>`;
-    //         telaPessoas.innerHTML += defaultHTML;
-    //     }
-    //     else{
-    //         let defaultHTML = `          
-    //     <div id="msgError">
-    //         <h2>Nenhum Resultado Encontrado</h2>
-    //     </div>`
-    //         telaPessoas.innerHTML += defaultHTML;
-    //     }
-    // })
 });
+//     // usuariosObjeto.forEach(eachUsers => {
+//     //     if(eachUsers.nome === inputPessoas.value){
+//     //         let defaultHTML = `
+//     //     <div class="resultadoPesquisa">
+//     //         <span id="info">
+//     //             <a href="#"><img src="assets/img/icones de interface/do-utilizador (2).png" /></a>
+//     //             <h2>${eachUsers.nome}</h2>
+//     //         </span>
+//     //         <button id="adicionarPessoa">Adicionar</button>
+//     //     </div>`;
+//     //         telaPessoas.innerHTML += defaultHTML;
+//     //     }
+//     //     else{
+//     //         let defaultHTML = `
+//     //     <div id="msgError">
+//     //         <h2>Nenhum Resultado Encontrado</h2>
+//     //     </div>`
+//     //         telaPessoas.innerHTML += defaultHTML;
+//     //     }
+//     // })
+// });
 
